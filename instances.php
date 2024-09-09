2fd47c0535d0575042685f2b1d2495f7b4442ab8
sarthac <hi99@tutanota.com>
2024-09-09 17:44:19 +0530

    intial

---
+++ instances.php
@@ -0,0 +1,46 @@
+<?php
+require_once "misc/header.php";
+
+?>
+
+
+<table>
+    <tr>
+        <th>Place</th>
+        <td>Europe/Germany</td>
+    </tr>
+    <tr>
+        <th>Clearnet</th>
+        <td><a href="https://t.librey.org">https://t.librey.org</a></td>
+    </tr>
+
+    <tr>
+        <th>Tor</th>
+        <td><a href="http://t5nctnrmrtooc3jlt2ygm2bvdvktftdvmlvzxaqwgkxhswntyxo6lyyd.onion"> http://t5nctnrmrtooc3jlt2ygm2bvdvktftdvmlvzxaqwgkxhswntyxo6lyyd.onion</a></td>
+    </tr>
+
+    <tr>
+        <th>I2P</th>
+        <td><a href="http://zrwytny3yzgeeo7wfvbjwbanksvwuud6s6xewfb5cchsmtupubwq.b32.i2p">http://zrwytny3yzgeeo7wfvbjwbanksvwuud6s6xewfb5cchsmtupubwq.b32.i2p</a></td>
+    </tr>
+
+</table>
+
+<br><br><br><br>
+
+
+<table>
+    <tr>
+        <th>Place</th>
+        <td>NorthAmerica/US</td>
+    </tr>
+    <tr>
+        <th>Clearnet</th>
+        <td><a href="https://trnt.librey.org">https://trnt.librey.org</a></td>
+    </tr>
+
+    <tr>
+        <th>Tor</th>
+        <td><a href="http://po2fyxzl3tadffsiotc3zvgrb4hgex7xpvlutah3um6ick6cneidkrid.onion">http://po2fyxzl3tadffsiotc3zvgrb4hgex7xpvlutah3um6ick6cneidkrid.onion</a></td>
+    </tr>
+</table>
