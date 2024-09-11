a2c6f8d462751d347ea18fcab63f6b5c7b3999a5
sarthac <hi99@tutanota.com>
2024-09-11 18:49:09 +0530

    added footer.php

---
+++ misc/footer.php
@@ -0,0 +1,16 @@
+<?php
+include_once "misc/header.php";
+
+?>
+
+<div class="footer">
+    <div>
+        <h3><a href="./">trntSearch</a></h3>
+    </div>
+
+    <div>
+        <a href="instances.php">Tor & I2P</a>
+        <a target="_blank" href="https://github.com/sarthac/trntSearch">Source Code</a>
+    </div>
+
+</div>
