b7a9aa58ea133b9288409ff91595e7a4fb3f341a
sarthac <hi99@tutanota.com>
2024-09-13 21:16:12 +0530

    added

---
+++ api.php
@@ -0,0 +1,50 @@
+<?php
+$query = htmlspecialchars($_REQUEST["query"]);
+$site = $_REQUEST["site"];
+$category = $_REQUEST["category"];
+
+$results = array();
+
+switch ($site) {
+
+    case "yts":
+        include "yts.php";
+        $results = get_yts_results($query);
+        break;
+
+    case "academic_torrents":
+        include "academic_torrents.php";
+        $results = search_by_name($query, $category);
+        break;
+
+    case "piratebay":
+        include "piratebay.php";
+        $results = get_thepiratebay_results($query);
+        break;
+
+    case "1337x":
+        include "1337x.php";
+        $response = file_get_contents($_1337x_url);
+        $results = get_1337x_results($response);
+        break;
+
+    case "rarbg":
+        include "rarbg.php";
+        $results = get_rarbg_results($query);
+        break;
+
+    case "eztvx":
+        include "eztvx.php";
+        include "omdbapi.php";
+        $omdb_id = get_omdbapi_id($query);
+        $results = get_eztvx_results($omdb_id);
+        break;
+
+    default:
+        include "yts.php";
+        $results = get_yts_results($query);
+        break;
+}
+
+header("Content-Type: application/json");
+echo json_encode($results);
---
+++ guide.php
@@ -0,0 +1,58 @@
+<?php
+include "misc/header.php";
+?>
+
+<style>
+    h3 {
+        font-size: 20px;
+    }
+
+    p {
+        color: rgb(255, 255, 255, .75);
+    }
+</style>
+
+
+<h3>Those who are unfamiliar with torrent and getting a zero results on trntSearch here is guide for you</h3>
+
+<p>in trntSearch you find multiple option in drop down menu which are yts, academic-torrent, 1337x, piratebay, rarbg and eztvx.These are sites which you get results from them. trntSearch work as a proxy that aims to give results without ads, trackers, give you declutter experience. if you search tv series in yts it will give you 0 results because yts only provide movies.
+    here are some info on each sites.
+</p>
+
+<hr>
+
+
+
+<h3>yts</h3>
+<p>yts provide all sort of movies ranging from 1080p upto 4k including web, bluray and 3d. If you would like to search movies than yts is a best option to search into.
+</p>
+
+<hr>
+
+
+<h3>academic-torrents</h3>
+<p>academic-torrents provide academic level contents, wide range of datasets across various fields, including computer-science, physics, engineering and more.
+</p>
+
+<hr>
+
+
+<h3>piratebay & 1337x</h3>
+<p>both site provide all sort of digital media i.e movie, tv shows, music, pc games, software and more</p>
+
+<hr>
+
+
+<h3>rarbg</h3>
+<p>rarbg provide all stremable media i.e movie, tv shows and more</p>
+
+<hr>
+
+
+
+<h3>eztvx</h3>
+<p style="padding-bottom: 40px;">eztvx only provide a collection of tv shows</p>
+
+
+<?php
+include "misc/footer.php";
