4d03546b79625cb7b52964b71b84bc7c3bda113d
sarthac <hi99@tutanota.com>
2024-09-13 23:21:18 +0530

    fix footer and academic-torrent paragraph

--- academic_torrents.php
+++ academic_torrents.php
@@ -5,61 +5,6 @@ $xml = simplexml_load_file('https://academictorrents.com/database.xml');
 
 $items = $xml->channel->item;
 
-function search_by_name_bk($query, $category)
-{
-    global $config, $xml, $items;
-
-    // example "data structure"
-    $query = ucwords(strtolower($query));
-    $results = array();
-
-
-    foreach ($items as $item) {
-        $title = (string) $item->title;
-        // converting for search because it case sensitive
-        $title_convert = ucwords(strtolower($title));
-        $file_category = (string) $item->category;
-        $description = (string) $item->description;
-        $infohash = (string) $item->infohash;
-        $magnet = "magnet:?xt=urn:btih:$infohash&dn=$title" . $config->academic_torrent_trackers;
-        $size = $item->size;
-        $size = human_filesize($size);
-
-
-        $array_title = explode(" ", $title_convert);
-
-        foreach ($array_title as $word_compare) {
-            if ($query == $word_compare) {
-                if ($category == "All") {
-                    array_push(
-                        $results,
-                        array(
-                            "title" => htmlspecialchars($title),
-                            "category" => $file_category,
-                            "description" => htmlspecialchars($description),
-                            "magnet" => $magnet,
-                            "size" => $size,
-                        )
-                    );
-                } elseif ($category == $file_category) {
-                    array_push(
-                        $results,
-                        array(
-                            "title" => htmlspecialchars($title),
-                            "category" => $file_category,
-                            "description" => htmlspecialchars($description),
-                            "magnet" => $magnet,
-                            "size" => $size,
-                        )
-                    );
-                }
-                break;
-            }
-        }
-    }
-    return $results;
-}
-
 function search_by_name($query, $category)
 {
     global $config, $xml, $items;
@@ -176,7 +121,7 @@ function print_academic_torrents_results($results, $query)
             echo "<div class = \"margin-bottom-50\">";
             echo "<h2>$title</h2>";
             if ($result["description"] != null) {
-                echo "<p> $description </p>";
+                echo "<p class=\"para\"> $description </p>";
             }
             echo "<table>";
             echo "<tr>";
