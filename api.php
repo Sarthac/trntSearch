158809f8bed4e690a2eda160f96ecc4ec5b2bae9
sarthac <hi99@tutanota.com>
2024-10-04 20:38:23 +0530

    fix api & and change footer margin

--- api.php
+++ api.php
@@ -1,47 +1,47 @@
 <?php
 $query = htmlspecialchars($_REQUEST["query"]);
 $site = $_REQUEST["site"];
-$category = $_REQUEST["category"];
+// $category = $_REQUEST["category"];
 
 $results = array();
 
 switch ($site) {
 
     case "yts":
-        include "yts.php";
+        include "provider/yts.php";
         $results = get_yts_results($query);
         break;
 
     case "academic_torrents":
-        include "academic_torrents.php";
+        include "provider/academic_torrents.php";
         $results = search_by_name($query, $category);
         break;
 
     case "piratebay":
-        include "piratebay.php";
+        include "provider/piratebay.php";
         $results = get_thepiratebay_results($query);
         break;
 
     case "1337x":
-        include "1337x.php";
+        include "provider/1337x.php";
         $response = file_get_contents($_1337x_url);
         $results = get_1337x_results($response);
         break;
 
     case "rarbg":
-        include "rarbg.php";
+        include "provider/rarbg.php";
         $results = get_rarbg_results($query);
         break;
 
     case "eztvx":
-        include "eztvx.php";
+        include "provider/eztvx.php";
         include "omdbapi.php";
         $omdb_id = get_imdb_id($query);
         $results = get_eztvx_results($omdb_id);
         break;
 
     default:
-        include "yts.php";
+        include "provider/yts.php";
         $results = get_yts_results($query);
         break;
 }
