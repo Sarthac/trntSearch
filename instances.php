b688a04d453bbf2b07d1379430141e6d3208e962
sarthac <hi99@tutanota.com>
2024-10-06 18:00:01 +0530

    fix instances.php width

--- instances.php
+++ instances.php
@@ -3,58 +3,60 @@ include "misc/header.php";
 
 ?>
 
+<div>
 
-<table class="instance-table">
-    <tr>
-        <th>Hosted</th>
-        <td>Europe/Germany</td>
-    </tr>
-    <tr>
-        <th>Clearnet</th>
-        <td><a href="https://t.librey.org">https://t.librey.org</a></td>
-    </tr>
+    <table class="instance-table margin-top-50">
+        <tr>
+            <th>Hosted</th>
+            <td>Europe/Germany</td>
+        </tr>
+        <tr>
+            <th>Clearnet</th>
+            <td><a href="https://t.librey.org">https://t.librey.org</a></td>
+        </tr>
 
-    <tr>
-        <th>Tor</th>
-        <td><a href="http://t5nctnrmrtooc3jlt2ygm2bvdvktftdvmlvzxaqwgkxhswntyxo6lyyd.onion"> http://t5nctnrmrtooc3jlt2ygm2bvdvktftdvmlvzxaqwgkxhswntyxo6lyyd.onion</a></td>
-    </tr>
+        <tr>
+            <th>Tor</th>
+            <td><a href="http://t5nctnrmrtooc3jlt2ygm2bvdvktftdvmlvzxaqwgkxhswntyxo6lyyd.onion"> http://t5nctnrmrtooc3jlt2ygm2bvdvktftdvmlvzxaqwgkxhswntyxo6lyyd.onion</a></td>
+        </tr>
 
-    <tr>
-        <th>I2P</th>
-        <td><a href="http://zrwytny3yzgeeo7wfvbjwbanksvwuud6s6xewfb5cchsmtupubwq.b32.i2p">http://zrwytny3yzgeeo7wfvbjwbanksvwuud6s6xewfb5cchsmtupubwq.b32.i2p</a></td>
-    </tr>
+        <tr>
+            <th>I2P</th>
+            <td><a href="http://zrwytny3yzgeeo7wfvbjwbanksvwuud6s6xewfb5cchsmtupubwq.b32.i2p">http://zrwytny3yzgeeo7wfvbjwbanksvwuud6s6xewfb5cchsmtupubwq.b32.i2p</a></td>
+        </tr>
 
-</table>
+    </table>
 
-<table class="instance-table">
-    <tr>
-        <th>Hosted</th>
-        <td>NorthAmerica/US</td>
-    </tr>
-    <tr>
-        <th>Clearnet</th>
-        <td><a href="https://trnt.librey.org">https://trnt.librey.org</a></td>
-    </tr>
+    <table class="instance-table margin-top-50">
+        <tr>
+            <th>Hosted</th>
+            <td>NorthAmerica/US</td>
+        </tr>
+        <tr>
+            <th>Clearnet</th>
+            <td><a href="https://trnt.librey.org">https://trnt.librey.org</a></td>
+        </tr>
 
-    <tr>
-        <th>Tor</th>
-        <td><a href="http://po2fyxzl3tadffsiotc3zvgrb4hgex7xpvlutah3um6ick6cneidkrid.onion">http://po2fyxzl3tadffsiotc3zvgrb4hgex7xpvlutah3um6ick6cneidkrid.onion</a></td>
-    </tr>
-</table>
+        <tr>
+            <th>Tor</th>
+            <td><a href="http://po2fyxzl3tadffsiotc3zvgrb4hgex7xpvlutah3um6ick6cneidkrid.onion">http://po2fyxzl3tadffsiotc3zvgrb4hgex7xpvlutah3um6ick6cneidkrid.onion</a></td>
+        </tr>
+    </table>
 
-<table class="instance-table">
-    <tr>
-        <th>Tor</th>
-        <td><a href="http://vyurzzaj3zi4gblkfabqrioyo5rfi5pczjjn3decuvjw2bqvbowhfwyd.onion">http://vyurzzaj3zi4gblkfabqrioyo5rfi5pczjjn3decuvjw2bqvbowhfwyd.onion</a></td>
-    </tr>
+    <table class="instance-table margin-top-50">
+        <tr>
+            <th>Tor</th>
+            <td><a href="http://vyurzzaj3zi4gblkfabqrioyo5rfi5pczjjn3decuvjw2bqvbowhfwyd.onion">http://vyurzzaj3zi4gblkfabqrioyo5rfi5pczjjn3decuvjw2bqvbowhfwyd.onion</a></td>
+        </tr>
 
-    <tr>
-        <th>I2P</th>
-        <td><a href="http://tb5t4jzxp4uu67jrm7iqizs4cms26mz7qefj2nz6roxhhw4p22ba.b32.i2p">http://tb5t4jzxp4uu67jrm7iqizs4cms26mz7qefj2nz6roxhhw4p22ba.b32.i2p</a></td>
-    </tr>
-</table>
+        <tr>
+            <th>I2P</th>
+            <td><a href="http://tb5t4jzxp4uu67jrm7iqizs4cms26mz7qefj2nz6roxhhw4p22ba.b32.i2p">http://tb5t4jzxp4uu67jrm7iqizs4cms26mz7qefj2nz6roxhhw4p22ba.b32.i2p</a></td>
+        </tr>
+    </table>
 
 
+</div>
 
 
 <?php
